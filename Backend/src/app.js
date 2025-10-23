9aaecb2c7ef5561c128ed4e7427e46ed6d02c008
Dhruv102004 <dhruvagrawals@gmail.com>
2025-10-23 18:34:35 +0530

    Add buyer routes and controller for product recommendations, categories and search functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,8 +15,10 @@ app.use(cookieParser());
 
 import userRouter from './routes/user.route.js'
 import sellerRouter from './routes/seller.route.js'
+import buyerRouter from './routes/buyer.route.js'
 
 app.use("/users", userRouter)
 app.use("/seller", sellerRouter)
+app.use("/buyer", buyerRouter)
 
 export { app };

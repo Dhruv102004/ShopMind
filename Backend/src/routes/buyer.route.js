ac87a7c0513a7f7c33f846eb4d56034b6722922d
Dhruv102004 <dhruvagrawals@gmail.com>
2025-10-27 18:52:38 +0530

    Add getProduct controller and route

--- Backend/src/routes/buyer.route.js
+++ Backend/src/routes/buyer.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import {
     getRecommendedProducts,
+    getProduct,
     searchProducts,
     getProductsByCategories,
     getCategories
@@ -10,6 +11,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js"
 const router = Router()
 
 router.route("/get-recommended-products").get(verifyJWT, getRecommendedProducts)
+router.route("/get-product/:productId").get(verifyJWT, getProduct)
 router.route("/search-products").get(verifyJWT, searchProducts)
 router.route("/get-product-by-categories").get(verifyJWT, getProductsByCategories)
 router.route("/get-categories").get(verifyJWT, getCategories)
